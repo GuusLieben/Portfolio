0852d703551f5a364d208a2164d387af931467ab
Guus Lieben <guus@xendox.com>
2023-01-10 20:00:31 +0100

    Add cursor balls with appropriate touch and 'a' hooks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FocusElementComponent } from './focus-element/focus-element.component';
+import { CursorComponent } from './cursor/cursor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FocusElementComponent
+    FocusElementComponent,
+    CursorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cursor/cursor.component.ts
@@ -0,0 +1,67 @@
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {gsap} from "gsap";
+import {deviceType} from "detect-it";
+
+@Component({
+  selector: 'app-cursor',
+  templateUrl: './cursor.component.html',
+  styleUrls: ['./cursor.component.scss']
+})
+export class CursorComponent implements AfterViewInit {
+
+  @Input()
+  shake?: HTMLSpanElement;
+
+  @ViewChild('ballBig')
+  bigBall?: ElementRef;
+
+  @ViewChild('ballSmall')
+  smallBall?: ElementRef;
+
+  ngAfterViewInit(): void {
+    if (deviceType === 'mouseOnly') {
+      document.body.addEventListener('mousemove', this.onMouseMove.bind(this));
+      document.body.classList.add('no-cursor');
+      this.adaptLinks();
+      this.adaptElement(this.shake!, 1.5);
+    }
+    else {
+      this.bigBall!.nativeElement.style.display = 'none'
+      this.smallBall!.nativeElement.style.display = 'none'
+    }
+  }
+
+  onMouseMove(event: MouseEvent) {
+    if (this.bigBall) {
+      gsap.to(this.bigBall.nativeElement, .4, {
+        x: event.clientX - 15,
+        y: event.clientY - 15
+      });
+    }
+
+    if (this.smallBall) {
+      gsap.to(this.smallBall.nativeElement, .1, {
+        x: event.clientX - 5,
+        y: event.clientY - 7
+      });
+    }
+  }
+
+  private adaptLinks() {
+    const links = document.body.getElementsByTagName('a');
+    for (let i = 0; i < links.length; i++) this.adaptElement(links[i], 4);
+  }
+
+  private adaptElement(element: HTMLElement, scale: number) {
+    element.addEventListener('mouseenter', () => {
+      gsap.to(this.bigBall!.nativeElement, .3, {
+        scale: scale
+      })
+    });
+    element.addEventListener('mouseleave', () => {
+      gsap.to(this.bigBall!.nativeElement, .3, {
+        scale: 1
+      })
+    });
+  }
+}
