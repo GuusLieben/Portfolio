8f69594ef95bfe2b4bfa18ef91e004609640372c
Guus Lieben <guus@xendox.com>
2023-01-11 19:17:27 +0100

    Timeline layout and styling

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -10,18 +10,27 @@ export class TimelineComponent {
   @Input()
   entries: TimelineEntry[] = [];
 
+  formatPeriod(period: TimelinePeriod) {
+    return period.start + (period.end ? (' - ' + period.end) : '');
+  }
 }
 
 export type TimelineEntry = {
   name: string;
   logo: string;
-  location: string;
+  location?: string;
   activities: TimelineActivity[];
+  period?: TimelinePeriod;
 }
 
 export type TimelineActivity = {
   title: string;
-  period: { start: string; end?: string; }
-  description: string;
-  keywords: string[];
+  period: TimelinePeriod;
+  description?: string;
+  keywords?: string[];
+}
+
+export type TimelinePeriod = {
+  start: string;
+  end?: string;
 }
